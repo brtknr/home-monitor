7489ed1f41c16b7297d2ceb7c5309b002bcb4eec
Bharat Kunwar <brtknr@bath.edu>
2019-09-05 19:22:32 +0100

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /opt/poll-bme280
+source ./bin/activate
+pip install -r requirements.txt
+ln -s /opt/poll-bme280/poll-bme280.service /etc/systemd/system/poll-bme280.service
+systemctl enable poll-bme280.service
+systemctl start poll-bme280.service
+
