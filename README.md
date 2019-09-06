3aa4cd88524c196c7686c9f7eaa026d18782364f
Bharat Kunwar <brtknr@bath.edu>
2019-09-06 01:02:14 +0100

    Add timeout for requests and batch/buffer writes

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-Steps:
+Install InfluxDB:
+
+	sudo apt install influxdb
+	sudo systemctl enable influxdb
+	sudo systemctl start influxdb
+	sudo systemctl status influxdb
+
+Create `influxdb` user and fill in `secret.py` with admin username and password:
+
+	influx -execute "CREATE USER <username> WITH PASSWORD '<password>' WITH ALL PRIVILEGES"
+	cp secret.py{.sample,}
+
+At this point, you want want to set `auth-enabled = true` inside
+`/etc/influxdb/influxdb.conf` if you want to expose your database to the web.
+
+Now you are ready to start your polling service:
 
 	git clone github.com/brtknr/poll-bme280
 	sudo mv poll-bme-280 /opt/poll-bme280
--- poll.py
+++ poll.py
@@ -1,5 +1,5 @@
-import influxdb
 import requests
+import influxdb
 import json
 import time
 import datetime
@@ -8,19 +8,27 @@ import sys
 from secret import credentials # template credentals provided in secret.py.sample
 
 measurement = 'bme280'
-endpoint  = 'http://192.168.8.110/bme280'
+endpoint  = 'http://192.168.8.%i/bme280'
+rooms = [100, 110]
 
 client = influxdb.InfluxDBClient(**credentials)
 client.create_database(credentials.get('database'))
 
+points = list()
 while True:
+    for room in rooms:
+        try:
+            fields = json.loads(requests.get(endpoint%room, timeout=1).content)
+            cur_time = datetime.datetime.utcnow().isoformat().split('.')[0]
+            point = dict(measurement=measurement, time=cur_time, fields=fields, tags=dict(room=room))
+            print(point)
+            points.append(point)
+        except Exception as e:
+            print(e)
     try:
-        fields = json.loads(requests.get(endpoint).content)
-        cur_time = datetime.datetime.utcnow().isoformat().split('.')[0]
-        point = dict(measurement=measurement, time=cur_time, fields=fields) 
-        print(point)
-        client.write_points([point])
+        client.write_points(points)
+        points.clear()
     except Exception as e:
-        print('Error', e)
+        print(e)
     sys.stdout.flush()
     time.sleep(10)
