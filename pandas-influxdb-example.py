f031a018135761e90273978a3c439c62d70d8158
Bharat Kunwar <brtknr@bath.edu>
2019-09-25 21:52:12 +0100

    Simplify script

--- pandas-influxdb-example.py
+++ pandas-influxdb-example.py
@@ -1,11 +1,9 @@
-import pandas
 import influxdb
 from config import credentials, measurement
 client = influxdb.DataFrameClient(**credentials)
 query = ('select * from %s'
-         ' where time < now() - 90m and time > now() - 100m'
+         ' where time < now() - 90m'
+         ' and time > now() - 100m'
          ' limit 10' % measurement)
-
-if __name__ == '__main__':
-    df = client.query(query).get(measurement)
-    print(df.describe())
+df = client.query(query).get(measurement)
+print(df.describe())
