8ca3359bc8b6132c8be74a3775e7e01b7291e037
Bharat Kunwar <brtknr@bath.edu>
2019-09-25 17:56:33 +0100

    Use async and aiohttp for async requests

--- poll.py
+++ poll.py
@@ -4,31 +4,50 @@ import json
 import time
 import datetime
 import sys
-
 from secret import credentials # template credentals provided in secret.py.sample
 
-measurement = 'bme280'
-endpoint  = 'http://192.168.8.%i/bme280'
-rooms = [100, 110, 120]
 
 client = influxdb.InfluxDBClient(**credentials)
 client.create_database(credentials.get('database'))
 
-points = list()
-while True:
+import aiohttp
+import asyncio
+
+async def fetch(session, url):
+    try:
+        async with session.get(url, timeout=5) as response:
+            return await response.text()
+    except Exception as e:
+        print(e)
+        return False
+
+async def main():
+    measurement = 'bme280'
+    endpoint  = 'http://192.168.8.%i/bme280'
+    rooms = [100, 110, 120]
     cur_time = datetime.datetime.utcnow().isoformat().split('.')[0]
-    for room in rooms:
-        try:
-            fields = json.loads(requests.get(endpoint%room, timeout=5).content)
-            point = dict(measurement=measurement, time=cur_time, fields=fields, tags=dict(room=room))
-            print(point)
-            points.append(point)
-        except Exception as e:
-            print(e)
+    tasks = []
+    async with aiohttp.ClientSession() as session:
+        for room in rooms:
+            url = endpoint % room
+            tasks.append(fetch(session, url))
+        htmls = await asyncio.gather(*tasks)
+        points = []
+        for html in htmls:
+            if html:
+                fields = json.loads(html)
+                point = dict(measurement=measurement, time=cur_time, fields=fields, tags=dict(room=room))
+                print(point)
+                points.append(point)
     try:
         client.write_points(points)
-        points.clear()
+        print('Wrote %i point(s)' % len(points))
     except Exception as e:
         print(e)
     sys.stdout.flush()
     time.sleep(10)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    while True:
+        loop.run_until_complete(main())
