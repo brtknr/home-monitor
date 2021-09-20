57eb8011b0f0e5804520396b939f0aefbd7f34a9
Bharat Kunwar <brtknr@bath.edu>
2021-09-20 13:08:50 +0100

    Use certbot in another directory

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(dash.rawn.uk)
 rsa_key_size=4096
-data_path="./certbot"
+data_path="../certbot"
 email="brtknr@bath.edu" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
